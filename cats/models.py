dba7719bd827a7aa3f4b170b398222d12d89cb74
Никита Ишмекеев <nikita.ishmekeev.2003@gmail.com>
2023-03-29 17:28:09 +0500

    1

---
+++ cats/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=16)
+    color = models.CharField(max_length=16, choices=CHOICES)
+    birth_year = models.IntegerField()
+    owner = models.ForeignKey(
+        Owner, related_name='cats', on_delete=models.CASCADE, blank=True,
+        null=True
+    )
+    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
+
+    def __str__(self):
+        return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'
---
+++ cats/serializers.py
@@ -0,0 +1,61 @@
+import datetime as dt
+
+import webcolors
+from rest_framework import serializers
+
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('для этого цвета нет имени')
+        return data
+
+
+class CatSerializer(serializers.ModelSerializer):
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(**achievement)
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat
+            )
+        return cat
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'second_name', 'cats')
